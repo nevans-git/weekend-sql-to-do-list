53bd85e533d5edb1c796fb64ace3699bc50921ff
Nick Evans <nickevans20@outlook.com>
2020-08-08 16:55:57 -0500

    Setup router side GET, POST, PUT, pool query, and client side GET

--- server/public/routes/todo.router.js
+++ server/public/routes/todo.router.js
@@ -23,13 +23,73 @@ pool.on("error", (error) => {
 });
 
 // Setup GET
+toDoRouter.get('/', (req, res) => {
+    let queryText = `SELECT * FROM "tasks" ORDER BY "id";`;
+    pool.query(queryText).then(result => {
+        // Sends back the results in an object
+        res.send(result.rows);
+
+    }).catch(error => {
+        console.log('error getting tasks', error);
+        res.sendStatus(500);       
+    })
+})
 
 
 // Setup POST
+toDoRouter.post('/', (req, res) => {
+    let queryText = `INSERT INTO "tasks" ("task", "status")
+    VALUES ($1, $2);
+    `;
 
+    pool.query(queryText, [
+        req.body.task,
+        req.body.status
+    ]).then(result => {
+        res.sendStatus(201);
+    }).catch(error => {
+        console.log('error posting ');
+        res.sendStatus(500);
+        
+    })
+})
 
 
 // Setup PUT
+toDoRouter.put('/:id', (req, res) => {
+    let todo = req.body;
+    let id = req.params.id;
+
+    console.log(`Updating todo ${id} with`, todo);
+
+    let queryText = '';
+    if(req.body.status === 'D'){ // D is for Done
+        queryText = `
+        UPDATE "tasks"
+        SET "status" = True
+        WHERE "id" = $1
+        `
+    }
+    else if (req.body.status === 'ND'){ // ND is for Not Done
+        queryText = `
+        UPDATE "tasks"
+        SET "status" = False
+        Where "id" = $1`
+    }
+
+    pool.query(queryText, [id]).then((result) => {
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log('error in post ', error);
+        res.sendStatus(500);
+        
+    })
+    
+})
 
 
-// Setup Delete
+// Setup Delete (complete once you setup delete button on client-side)
+
+
+
+modules.exports = toDoRouter;
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -8,3 +8,38 @@ function onReady(){
     $() // Setup click listener for each button
     
 } // end onReady
+
+function setupClickListeners(){
+
+}
+
+
+function getTasks(){
+    console.log('in getTasks');
+
+    // ajax calls to server to get tasks
+    $.ajax({
+        method: 'GET',
+        url: '/tasksToDo'
+    }).then( function (response){
+        console.log(response);
+        const taskList = response;
+        $('#viewTasks').empty();
+        for(let toDoTask of taskList){
+            if(toDoTask.status == true){
+                toDoTask.status = 'D'
+            }
+            else if (toDoTask.status == false){
+                toDoTask.status = 'ND'
+            }
+
+            $('#viewTasks').append(`<tr data-task-id="${toDoTask.id}">
+                <td>${toDoTask.task}</td>
+                <td>${toDoTask.status}</td>
+            </tr>`)
+        } // end of for loop
+    }).catch( function (error) {
+        console.log('error in task GET', error); 
+    })
+    
+} // end getTasks
